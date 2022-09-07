b860e31576e4569cd09c099c372f7313f59b17a5
Daniel Levi <Danielevi@protonmail.com>
2022-09-07 12:38:09 +0300

    change before REadme

--- js/app.js
+++ js/app.js
@@ -84,6 +84,7 @@ form.addEventListener("submit", e => {
     e.preventDefault();
     alert("thanks for submitting your details");
 });
+
 let removeActive = () => {
     let activea = document.querySelector("a.active");
     if(activea !=null){
@@ -96,17 +97,36 @@ function makeActive(){
     for (const section of landing) {
         const box = section.getBoundingClientRect();
         //Find a value that works best, but 150 seems to be a good start.
-        removeActive();
         if (box.top <= 150 && box.bottom >= 150) {
+         removeActive();
         //apply active state on current section and corresponding Nav link
-        let id = section.getAttribute("id");
-        document.querySelector(`[href="#${id}"]`).classList.add("active")
+         let id = section.getAttribute("id");
+         const activeClass = document.querySelector(`a[href="#${id}"]`)
+         activeClass.classList.add("active");
+        
         }
         
      }
   }
 
   document.addEventListener("scroll", function() { makeActive();});
+
+  const hambuger = document.querySelector("#menu");
+  hambuger.addEventListener("click", e => {
+    const select = document.querySelector("#navbar__list");
+    select.classList.toggle("show");
+    const allAnchors = document.querySelectorAll("nav a");
+    allAnchors.forEach(anchor =>{
+        anchor.addEventListener("click", ()=>{
+            select.classList.toggle("show");
+        }
+        )
+    }
+        ) 
+    
+  }
+
+  )
 
 
 
